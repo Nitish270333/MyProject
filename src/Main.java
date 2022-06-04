d0a4167b043d63648fd53aa57ba5566bc7e905ae
Nitish270333 <nitishalonso@gmail.com>
2022-06-04 11:43:51 +0530

    branch 'issue1 first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 
 public class Main {
+	public static Greet() {
+		System.out.println("Good morning");
+	}
 	public static void main(String[] args) {
 		System.out.println("This is the main class");
 	}
